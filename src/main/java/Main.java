03a470457ebc029f29af314014c08afc7b48b6cf
Perfors <nienke.perfors@sogeti.com>
2019-12-03 08:11:39 +0100

    oplossing Day2 part1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 import Day1.Day1;
+import Day2.Day2;
 
 public class Main {
 
     public static void main(String args[]){
-        System.out.println("the answer is: " + Day1.part2());
-        System.out.println("het antwoord was: 5148724");
+        System.out.println("the answer is: " + Day2.part1());
+
     }
 }
--- src/main/java/Reader/fileReader.java
+++ src/main/java/Reader/fileReader.java
@@ -18,13 +18,37 @@ public class fileReader {
             e.printStackTrace();
         }
         ArrayList<Integer> list = new ArrayList<>();
-        while (s.hasNext()){
+        while (s.hasNext()) {
             list.add(parseInt(s.next()));
         }
         s.close();
 
         return list;
-    };
+    }
+
+    ;
+
+    public static ArrayList<Integer> fileStringToInt(String filePath) {
+        Scanner s = null;
+        try {
+            s = new Scanner(new File(fileLocation + filePath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (s.hasNext()) {
+            Scanner lineScanner = new Scanner(s.next());
+            lineScanner.useDelimiter(",");
+            while (lineScanner.hasNext()) {
+                String part = lineScanner.next();
+                list.add(parseInt(part));
+            }
+        }
+        s.close();
+        return list;
+    }
 
 
 }
